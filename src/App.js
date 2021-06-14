9825798548814e5e5386ac2205a59de81acb239f
igalv <vilenskyigal@gmail.com>
2021-06-14 15:10:58 +0200

    components and handlers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import "./scss/main.scss";
+import React, { useState } from "react";
+import Data from "./data.json";
+import { FaBeer } from "react-icons/fa";
+import ProductList from "./components/ProductList";
 
 function App() {
-  return <div className="App">Hello</div>;
+  const [data, setData] = useState(Data);
+  const [userInput, setUserInput] = useState("");
+
+  const changeHandle = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const submitHandle = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <React.Fragment>
+      <h1>Welcome to our online store</h1>
+      <form onSubmit={submitHandle}>
+        <input type="text" onChange={changeHandle} value={userInput}></input>
+        <input type="submit" value="filter"></input>
+      </form>
+      <ProductList data={data} />
+    </React.Fragment>
+  );
 }
 
 export default App;
